1db2b36d4c6aa1ea73c2e0bc9e6c6b329daf0b77
tobigiwa <giwaoluwatobi@gmail.com>
2023-10-18 13:49:55 -0400

    added a .go file to include compulsory license header comment for every script
    Signed-off-by: tobigiwa <giwaoluwatobi@gmail.com>

---
+++ main.go
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2023, Oluwatobi Giwa
+ * All rights reserved.
+ *
+ * This software is licensed under the 3-Clause BSD License.
+ * See the LICENSE file or visit https://opensource.org/license/bsd-3-clause/ for details.
+ */
+
+package main
+
+func main() {
+
+}
