d9032bbd80729622ff25b87e08dd55ab53b53b20
tobigiwa <giwaoluwatobi@gmail.com>
2023-10-24 04:48:40 -0400

    still errors

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # NetWarden
 
-![](/Screenshot%20from%202023-10-23%2006-00-20.png)
--- trying_out.c
+++ trying_out.c
@@ -7,80 +7,44 @@
  * See the LICENSE file or visit https://opensource.org/license/bsd-3-clause/ for details.
  */
 
+#include <linux/bpf.h>
+#include <linux/ip.h>
+#include <bpf/bpf_helpers.h>
+#include <linux/if_ether.h>
+// #include <vmlinux.h>
 
+SEC("sk_skb")
+int packet_capture__(struct __sk_buff *skb)
+{
+    struct ethhdr eth;
+    struct iphdr ip;
 
-// #include <linux/bpf.h>
-// #include <linux/ip.h>
-// // #include <vmlinux.h>
-// #include <bpf/bpf_helpers.h>
-// #include <linux/if_ether.h>
+    __u32 src_ip;
+    __u32 dest_ip;
 
-// SEC("sk_skb")
-// int packet_capture__(struct __sk_buff *skb)
-// {
-//     struct ethhdr eth;
-//     struct iphdr ip;
+    __u32 pid;
 
-//     __u32 src_ip;
-//     __u32 dest_ip;
+    bpf_skb_load_bytes(skb, 0, &eth, sizeof(eth));
+    bpf_skb_load_bytes(skb, sizeof(eth), &ip, sizeof(ip));
 
-//     __u32 pid;
+    __u32 packet_size = skb->len;
 
-//     bpf_skb_load_bytes(skb, 0, &eth, sizeof(eth));
-//     bpf_skb_load_bytes(skb, sizeof(eth), &ip, sizeof(ip));
+    src_ip = ip.saddr;
+    dest_ip = ip.daddr;
 
-//     __u32 packet_size = skb->len;
+    __u64 uid_gid = bpf_get_current_uid_gid();
+    __u32 uid = uid_gid >> 32;
+    __u32 gid = uid_gid & 0xFFFFFFFF;
 
-//     src_ip = ip.saddr;
-//     dest_ip = ip.daddr;
+    char comm[16];
+    pid = bpf_get_current_pid_tgid() >> 32;
+    bpf_get_current_comm(&comm, sizeof(comm));
 
-//     __u64 uid_gid = bpf_get_current_uid_gid();
-//     __u32 uid = uid_gid >> 32;
-//     __u32 gid = uid_gid & 0xFFFFFFFF;
+    bpf_printk("Packet size: %u, Src IP: %u, Dest IP: %u ", packet_size, src_ip, dest_ip);
+    bpf_printk(", User ID: %u, Group ID: %u, PID: %u: ", uid, gid, pid);
+    bpf_printk(", Process name: %s\n", comm);
 
-//     char comm[16];
-//     pid = bpf_get_current_pid_tgid() >> 32;
-//     bpf_get_current_comm(&comm, sizeof(comm));
+    return 0;
+}
 
-//     bpf_printk("Packet size: %u, Src IP: %u, Dest IP: %u ", packet_size, src_ip, dest_ip);
-//     bpf_printk(", User ID: %u, Group ID: %u, PID: %u: ", uid, gid, pid);
-//     bpf_printk(", Process name: %s\n", comm);
-
-//     return 0;
-// }
-
-// char _license[] SEC("license") = "GPL";
-
-// #include <linux/bpf.h>
-// #include <linux/skbuff.h>
-// #include <linux/if_ether.h>
-// #include <linux/ip.h>
-// #include <linux/tcp.h>
-// #include <bpf/bpf_helpers.h>
-
-// SEC("sk_skb")
-// int packet_capture_another(struct __sk_buff *skb)
-// {
-//     struct ethhdr *eth = bpf_hdr_pointer(skb); // seems unsafe???
-//     struct iphdr *ip = (struct iphdr *)(eth + 1);
-
-//     __u32 packet_size = skb->len;
-
-//     __u32 src_ip = ip->saddr;
-//     __u32 dest_ip = ip->daddr;
-
-//     __u32 uid = bpf_get_current_uid_gid() >> 32;
-//     __u32 gid = bpf_get_current_uid_gid();
-
-//     struct task_struct *task = (struct task_struct *)bpf_get_current_task();
-//     __u32 pid = bpf_get_task_pid(task, 0);
-
-//     char comm[16];
-//     bpf_get_current_comm(comm, sizeof(comm));
-
-//     bpf_printk("Packet size: %u, Src IP: %u, Dest IP: %u, User ID: %u, Group ID: %u, PID: %u, Process name: %s\n", packet_size, src_ip, dest_ip, uid, gid, pid, comm);
-
-//     return 0;
-// }
-
-// char _license[] SEC("license") = "GPL";
+char _license[] SEC("license") = "GPL";
